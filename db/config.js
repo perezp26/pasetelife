0ce3fd7ad461427b7b383973cbcdb5b397409d02
Raul Perez <perezp@hotmail.com>
2023-07-26 08:51:12 -0600

    primer deploy 3

--- db/config.js
+++ db/config.js
@@ -3,6 +3,7 @@ const  { Sequelize  } = require('sequelize');
 const db = new Sequelize( process.env.DATABASE, process.env.USER, process.env.PASS ,{
     host: process.env.HOST,
     dialect : process.env.TYPEDATABASE,
+    port: process.env.PORT_DB,
     dialectOptions: {
         decimalNumbers: true
       }
